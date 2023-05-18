bc5ae301e094378eba8465d15e24f6f3b881b3de
Sneha <snehaagrawal0907@gmail.com>
2023-05-18 20:20:31 +0530

    added doublenavabr navigation

---
+++ ssmv-website/src/navbar/doubleNavbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState } from "react"
+import { FaFacebookSquare , FaTwitterSquare, FaYoutubeSquare} from 'react-icons/fa';
+import './navbar.css';
+  
+const DoubleNavbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  return (
+    <>
+        <nav className="navbar-top">
+            <div className="top-section-links">
+                <ul classname = "section-links-indiv">
+                    <li><a href = "/#"> Admission </a></li>
+                    <li><a href = "/#"> Alumni </a> </li>
+                    <li><a href = "/#"> Donate </a></li>
+                    <li><a href = "/#"> Grievances </a></li>
+                    <li><a href = "/#"> Login </a></li>
+                </ul>
+            </div>
+            {/* sociaol media links */}
+            <div className="social-media-icons">
+                <ul className="social-media-desktop">
+                    <li><a href="/#"> <FaFacebookSquare className="facebook"/></a></li>
+                    <li><a href="/#"><FaTwitterSquare className="twitter"/></a></li>
+                    <li><a href="/#"><FaYoutubeSquare className="youtube"/></a></li>
+                </ul>
+            </div>
+        </nav>
+        <nav className="navbar-bottom">
+            <div className="Logo">
+                <img src="" alt="loading"/>
+            </div>
+            <div className="main-menu">
+                <ul>
+                    <li><a href="/#">About Us</a></li>
+                    <li><a href="/#">Academics</a></li>
+                    <li><a href="/#">Administration</a></li>
+                    <li><a href="/#">Services</a></li>
+                    <li><a href="/#">Media</a></li>
+                </ul>
+            </div>
+            <div id='mobile'>
+                <i id='bars' onClick={() => { setIsNavExpanded(!isNavExpanded)}} className={isNavExpanded ? "fas fa-bars" : "fas fa-times"}></i>
+            </div>
+      </nav>
+    </>
+  );
+};
+
+export default DoubleNavbar;
+
