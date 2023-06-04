e015222311f874bcde65fac44eb61a83d6ce8a25
ssm-vidyalay <ssmvidyalay@gmail.com>
2023-06-04 09:53:46 +0530

    correcting Routing

---
+++ ssmv-website/src/pages/AboutUs/AboutUs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Aboutcarousel from './Aboutcarousel'
+import '../AboutUs/About.css'
+import Footer from '../../components/Footer/Footer'
+import DoubleNavbar from '../../components/header/doubleNavbar'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <DoubleNavbar/>
+      <div className='carousel'>
+        <Aboutcarousel/>
+        </div>
+        <div className='div'>
+            <h3 className='head'><b>ABOUT US</b></h3>
+        <div className='box2'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default AboutUs;
---
+++ ssmv-website/src/pages/Admission.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AdmnForm from '../components/Form/AdmnForm'
+import DoubleNavbar from '../components/header/doubleNavbar';
+
+
+const Admission = () => {
+  return (
+    <>
+        <DoubleNavbar />
+        <AdmnForm/>
+    </>
+  )
+}
+
+export default Admission;
