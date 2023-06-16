20ab9bddb8cfe5616edcf4602f4eeb6aed8d6462
ssm-vidyalay <ssmvidyalay@gmail.com>
2023-06-16 17:58:21 +0530

    trying HashRouter

--- ssmv-website/src/index.js
+++ ssmv-website/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, HashRouter} from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+   <HashRouter/>
       <App />
-    </BrowserRouter>
+   <HashRouter/>
   </React.StrictMode>
 );
 
